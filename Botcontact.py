029069833f9106fb31941f38f119604d4c73d77e
Mohamed39200Lo <110898938+Mohamed39200Lo@users.noreply.github.com>
2025-04-05 11:56:43 +0200

    Update Botcontact.py

--- Botcontact.py
+++ Botcontact.py
@@ -7,7 +7,7 @@ from app import server
 #from app import server
 
 # استبدل 'YOUR_BOT_TOKEN' ب token البوت الخاص بك
-bot = telebot.TeleBot('7674278704:AAFJu7kgwuRpG1YKnWdCYfO9J7Na8MXrblc')
+bot = telebot.TeleBot('7674278704:AAEiNgwTIQfKpyxKk5M353NN5UN8YRguhFo')
 
 # إعداد logging لتسجيل الأخطاء
 logging.basicConfig(filename='bot_errors.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
